44fae8dc18e2ab630b8f5d59f762b6ded8ca42a3
Solomon Ndung'u <ssouljabee@gmail.com>
2018-08-01 05:26:34 +0300

    My first changed commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function closeNav() {
+	document.getElementById("sidebar").style.width = "30px";
+	document.getElementById("content").style.marginLeft = "30px";
+	document.body.style.backgroundColor = "white";
+}
+
+function openNav() {
+	document.getElementById("sidebar").style.width = "250px";
+	document.getElementById("content").style.marginLeft = "250px";
+	document.body.style.backgroundColor = "rgb(232,231,230)"
+}
+
